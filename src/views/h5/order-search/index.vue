9179bc5292d18a21b86199cc817a1d4b0b5377d8
hutao <983210535@qq.com>
2024-01-16 17:40:59 +0800

    update

--- src/views/h5/order-search/index.vue
+++ src/views/h5/order-search/index.vue
@@ -9,7 +9,7 @@ const router = useRouter()
 const mobile = ref('')
 const getOrderDetail = () => {
   if (!mobile.value) {
-    ElMessage.warning('请输入手机号')
+    ElMessage.warning('请输入邮箱')
     return
   }
   orderDetailApi(mobile.value).then(res => {
@@ -29,7 +29,7 @@ const getOrderDetail = () => {
     </div>
     <div class="layui-card-body">
       <p style="color: #3C8CE7;font-size: 18px;font-weight: 700; text-align: center;margin: 20px 0">
-        输入购买时订单
+        输入邮箱查询订单
       </p>
       <div class="layui-tab">
         <div class="layui-tab-content" style="text-align: center">
@@ -48,6 +48,63 @@ const getOrderDetail = () => {
               </button>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="main-box">
+    <div class="pay-title">
+      <svg style="margin-bottom: -6px;" t="1603120404646" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1611" width="27" height="27">
+        <path d="M320.512 428.032h382.976v61.44H320.512zM320.512 616.448h320.512v61.44H320.512z" fill="#00EAFF" p-id="1612" data-spm-anchor-id="a313x.7781069.0.i3" class="selected"></path>
+        <path d="M802.816 937.984H221.184l-40.96-40.96V126.976l40.96-40.96h346.112l26.624 10.24 137.216 117.76 98.304 79.872 15.36 31.744v571.392l-41.984 40.96z m-540.672-81.92h500.736V345.088L677.888 276.48 550.912 167.936H262.144v688.128z" fill="#3C8CE7" p-id="1613" data-spm-anchor-id="a313x.7781069.0.i0" class=""></path>
+      </svg>
+      订单详情
+    </div>
+    <div class="layui-card-body info-box">
+      <div class="layui-row order-list">
+        <div class="layui-col-md4">
+          <ul class="info-ui">
+            <li>
+                <strong>订单编号:</strong>
+                N6WVSKU8P8ZYYPYK
+            </li>
+            <li>
+                <strong>订单名称:</strong>
+                哆唻A梦的百宝袋 x 1
+            </li>
+            <li>
+              <strong>购买数量:</strong> 
+              1
+            </li>
+            <li>
+              <strong>订单创建时间:</strong> 2023-12-07 21:52:02
+            </li>
+            <li>
+              <strong>下单邮箱:</strong> 
+              123456@qq.com
+            </li>
+          </ul>
+        </div>
+        <div class="layui-col-md4">
+          <ul class="info-ui">
+              <li>
+                <strong>订单类型:</strong>
+                <span class="small-tips tips-green">自动发货</span>
+              </li>
+              <li>
+                  <strong>实际支付价格:</strong>
+                  <span class="small-tips tips-green">￥0.00</span>
+              </li>
+              <li>
+                <strong>订单状态:</strong>
+                <span class="small-tips tips-green">已完成</span>
+              </li>
+              <li>
+                <strong>支付方式:</strong> 
+                支付宝当面付
+              </li>
+          </ul>
         </div>
       </div>
     </div>
